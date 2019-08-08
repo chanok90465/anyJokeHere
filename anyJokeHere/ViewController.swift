2e685057353da4f595e4ed7f5e776948093d2ef9
Chanok Hongsawat <chanok90465@gmail.com>
2019-08-08 14:04:15 +0700

    Initial Commit

---
+++ anyJokeHere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  anyJokeHere
+//
+//  Created by Chanok Hongsawat on 8/8/2562 BE.
+//  Copyright © 2562 Chanok Hongsawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
